ea1a968b0e1dfe8c5b2840569b3a35be96f2fff5
Lex Nelson <lexinelson597@gmail.com>
2021-08-31 09:43:00 -0400

    commit tree b83b43fa8498600948991b6b3d3b0a67b8410531

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,26 @@
 class BirdsController < ApplicationController
 
+  #PATCH/birds/:id
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else 
+      render json: {error: "Bird not found"}, status: :not_found 
+    end
+  end
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.update(bird_params)
+      render json: bird
+    else 
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -25,7 +46,7 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
